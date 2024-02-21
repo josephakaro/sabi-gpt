4172d6da16729c4e92a988823749baa87126d572
Joseph Akaro <josephmmakaro@gmail.com>
2024-02-21 11:48:25 +0200

    React Step up

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return(
+    <>
+    <div className="container">
+    <div className="sidebar">
+      <div className="new-chat">
+        <p>New Chat</p>
+      </div>
+    </div>
+    <div className="main">
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App;
